72863bf4e813a78fa9175d8b26d45db4dd229de2
shoaibkhan92 <shoaibkhan19920@gmail.com>
2024-08-22 12:49:50 +0530

    new repo create

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./pages/home";
+import Explore from "./pages/explore";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/explore",
+    element: <Explore />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
