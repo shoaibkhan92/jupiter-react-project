54daa7afa3a3a80837472eeed23e469a397a86e0
shoaibkhan92 <shoaibkhan19920@gmail.com>
2024-08-27 22:43:13 +0530

    new changes

--- src/pages/welcome-back.jsx
+++ src/pages/welcome-back.jsx
@@ -12,6 +12,24 @@ const features = [
     heading: " Dining",
   },
 ];
+const cardsdata = [
+  {
+    image: "/src/assets/images/icons/green-satar.png",
+    heading: "2% cashback on chosen category",
+  },
+  {
+    image: "/src/assets/images/icons/green-satar.png",
+    heading: "Cashback on all UPI & card spends",
+  },
+  {
+    image: "/src/assets/images/icons/green-satar.png",
+    heading: "Lifetime free no annual fee",
+  },
+  {
+    image: "/src/assets/images/icons/green-satar.png",
+    heading: "Welcome voucher worth ₹250",
+  },
+];
 
 export default function WelcomeBack() {
   return (
@@ -44,55 +62,24 @@ export default function WelcomeBack() {
               ))}
             </div>
           </div>
-
+          
           <div className="grid grid-cols-2 gap-2 mb-4">
+          {cardsdata.map((data, index) => (
             <div className="bg-beach border text-center border-tertiary border-b-3 rounded-lg py-2.5 px-3">
               <div className="flex text-left items-center gap-1">
-                <img
-                  src={"/src/assets/images/icons/green-satar.png"}
-                  className="bg-no-repeat bg-contain w-3 h-3"
+                <img src={data.image} className="bg-no-repeat bg-contain w-3 h-3"
                 />
                 <p className="text-xs font-semibold text-primary-black">
-                  2% cashback on chosen category
+                {data.heading}
                 </p>
               </div>
             </div>
-            <div className="gap-2 bg-beach border text-center border-tertiary border-b-3 rounded-lg py-2.5 px-3">
-              <div className="flex text-left items-center gap-1">
-                <img
-                  src={"/src/assets/images/icons/green-satar.png"}
-                  className="bg-no-repeat bg-contain w-3 h-3 inline-block"
-                />
-                <p className="text-xs font-semibold text-primary-black">
-                  Cashback on all UPI & card spends
-                </p>
-              </div>
-            </div>
-            <div className="bg-beach border text-center border-tertiary border-b-3 rounded-lg py-2.5 px-3">
-              <div className="flex text-left items-center gap-1">
-                <img
-                  src={"/src/assets/images/icons/green-satar.png"}
-                  className="bg-no-repeat bg-contain w-3 h-3"
-                />
-                <p className="text-xs font-semibold text-primary-black">
-                  Lifetime free no annual fee
-                </p>
-              </div>
-            </div>
-            <div className="gap-2 bg-beach border text-center border-tertiary border-b-3 rounded-lg py-2.5 px-3">
-              <div className="flex text-left items-center gap-1">
-                <img
-                  src={"/src/assets/images/icons/green-satar.png"}
-                  className="bg-no-repeat bg-contain w-3 h-3 inline-block"
-                />
-                <p className="text-xs font-semibold text-primary-black">
-                  Welcome voucher worth ₹250
-                </p>
-              </div>
-            </div>
+            ))}
           </div>
           <div className="bg-accent-alt-primary rounded text-center mb-4">
-            <button className="text-base font-bold leading-5 py-3.5 text-white">Resume application</button>
+            <button className="text-base font-bold leading-5 py-3.5 text-white">
+              Resume application
+            </button>
           </div>
         </div>
       </div>
